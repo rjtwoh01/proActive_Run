c656a775181f6b8bbcbdf047514a1c6302e17c79
Ryan Twohey <ryantwohey@Ryans-MacBook-Air.local>
2017-07-13 20:45:28 -0400

    Initial Commit

---
+++ proActive Run/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proActive Run
+//
+//  Created by Ryan Twohey on 7/13/17.
+//  Copyright © 2017 Ryan Twohey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
