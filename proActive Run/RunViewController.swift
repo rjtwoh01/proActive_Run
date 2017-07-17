758dcca8c4affbee2f6a7ba8abf6867d264d0b6a
rjtwoh01 <rjtwoh01@cardmail.louisville.edu>
2017-07-16 20:21:00 -0400

    Added more views. Lots of work remains to be done on them

---
+++ proActive Run/RunViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RunViewController.swift
+//  proActive Run
+//
+//  Created by Ryan Twohey on 7/16/17.
+//  Copyright © 2017 Ryan Twohey. All rights reserved.
+//
+
+import UIKit
+
+class RunViewController: UITableViewController {
+    
+}
